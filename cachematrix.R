bb951fce6d5da4a049c320fbb0931c1b12446423
jwfy <986450042@qq.com>
2014-12-18 09:41:23 +0800

    submit the code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## creates a special matrix and put the inverse value act as a cache variable
 makeCacheMatrix <- function(x = matrix()) {
-
+	selfInvMat <- NULL
+	# init the variable
+	
+	set <- function(y){
+		x <<- y
+		selfInvMat <<- NULL
+	}
+	# set function and set x value as the y
+	# and define the class of x as the matrix
+	
+	get <- function() x
+	# return the x
+	
+	setInv <- function(solve) selfInvMat <<- solve
+	# calculate the inverse of x and add to the cache
+	
+	getInv <- function() selfInvMat
+	# get the inverse value of x
+	
+	list(set=set, get=get, setInv=setInv, getInv=getInv)
+	# return all the function 
 }
 
 
-## Write a short comment describing this function
+## first get the inverse of the x,second analyse the value.
+## if the value has bean calculated we should return the value,
+## otherwise througu the "solve" function calculate the inverse of the x
+## and set the cache variable
+## finally return the value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	selfInvMax <- x$getInv()
+	# get the value 
+	
+	if(!is.null(selfInvMax)){
+		message("getting cached data")
+    	return(selfInvMax)
+	}
+	# cache has bean calculated ,return the value
+	
+	data <- x$get()
+	# set the matrix value
+	
+	selfInvMax <- solve(data, ...)
+	# calculated the inverse
+	
+	x$setInv(selfInvMax)
+	#save the value
+	
+	selfInvMax
+	# return the inverse
 }
